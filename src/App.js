c635461cd70971cb68b153ced635d2c22a007b3a
haruto17 <s1902272@sendai-nct.jp>
2023-07-02 22:19:39 +0900

    a

--- src/App.js
+++ src/App.js
@@ -1,75 +1,39 @@
-import { Textarea, Flex, Button } from "@mantine/core";
+import React, { useState } from "react";
+import { Textarea, Button, Flex, Container } from "@mantine/core";
 import { IconTrash, IconFileImport, IconFileExport } from "@tabler/icons-react";
-import "./App.css";
-import { useState } from "react";
-
-const TitleArea = (props) => {
-  const handleTitleChange = (event) => {
-    const value = event.target.value;
-    props.handleTitleValueChange(value);
-  };
-
-  return <Textarea onChange={handleTitleChange} mt={"md"} mb={"md"} radius={"md"} placeholder="Title" />;
-};
-
-const ContentsArea = (props) => {
-  const handleContentsChange = (event) => {
-    const value = event.target.value;
-    props.handleContentsValueChange(value);
-  };
-
-  return <Textarea onChange={handleContentsChange} mt={"md"} mb={"md"} radius={"md"} placeholder="Contents" autosize minRows={10} />;
-};
-
-function Control() {
-  const handleDelete = () => {
-    console.log("delete");
-  };
-
-  const handleExport = () => {
-    console.log("export");
-  };
-
-  const handleImport = () => {
-    console.log("import");
-  };
-
-  return (
-    <>
-      <Flex gap="md" justify="center" align="center">
-        <Button onClick={handleDelete} leftIcon={<IconTrash />} color="red">
-          Delete
-        </Button>
-        <Button onClick={handleExport} leftIcon={<IconFileExport />}>
-          Export
-        </Button>
-        <Button onClick={handleImport} leftIcon={<IconFileImport />}>
-          Import
-        </Button>
-      </Flex>
-    </>
-  );
-}
 
 function App() {
   const [title, setTitle] = useState("");
   const [contents, setContents] = useState("");
 
-  const handleTitleValueChange = (value) => {
-    setTitle(value);
-  };
-
-  const handleContentsValueChange = (value) => {
-    setContents(value);
+  const handleDelete = () => {
+    setTitle("");
+    setContents("");
   };
 
   return (
     <>
-      <TitleArea handleTitleValueChange={handleTitleValueChange} />
-      <ContentsArea handleContentsValueChange={handleContentsValueChange} />
-      <Control />
-      <pre>{title}</pre>
-      <pre>{contents}</pre>
+      <Container size="md">
+        <Textarea value={title} onChange={(event) => setTitle(event.target.value)} mt={"md"} mb={"md"} radius={"md"} placeholder="Title" />
+        <Textarea
+          value={contents}
+          onChange={(event) => setContents(event.target.value)}
+          mt={"md"}
+          mb={"md"}
+          radius={"md"}
+          placeholder="Contents"
+          autosize
+          minRows={10}
+        />
+      </Container>
+      <Flex gap="md" justify="center" align="center">
+        <Button onClick={handleDelete} leftIcon={<IconTrash />} color="red">
+          Delete
+        </Button>
+        <Button leftIcon={<IconFileExport />}>Export</Button>
+        <Button leftIcon={<IconFileImport />}>Import</Button>
+      </Flex>
+      ;
     </>
   );
 }
