877b9815a147dc77245b481877847d234d762f30
haruto17 <s1902272@sendai-nct.jp>
2023-07-02 20:05:26 +0900

    テキストエリアの内容を取得 -> 表示

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from "./logo.svg";
+import { Container, Textarea, Flex, Button } from "@mantine/core";
+import { IconTrash, IconFileImport, IconFileExport } from "@tabler/icons-react";
 import "./App.css";
+import { useState } from "react";
+import { render } from "@testing-library/react";
+
+const TitleArea = (props) => {
+  const handleTitleChange = (event) => {
+    const value = event.target.value;
+    props.handleTitleValueChange(value);
+  };
+
+  return <Textarea onChange={handleTitleChange} mt={"md"} mb={"md"} radius={"md"} placeholder="Title" />;
+};
+
+const ContentsArea = (props) => {
+  const handleContentsChange = (event) => {
+    const value = event.target.value;
+    props.handleContentsValueChange(value);
+  };
+
+  return <Textarea onChange={handleContentsChange} mt={"md"} mb={"md"} radius={"md"} placeholder="Contents" autosize minRows={10} />;
+};
+
+function Control() {
+  return (
+    <>
+      <Flex gap="md" justify="center" align="center">
+        <Button leftIcon={<IconTrash />} color="red">
+          Delete
+        </Button>
+        <Button leftIcon={<IconFileExport />}>Export</Button>
+        <Button leftIcon={<IconFileImport />}>Import</Button>
+      </Flex>
+    </>
+  );
+}
 
 function App() {
+  const [title, setTitle] = useState("");
+  const [contents, setContents] = useState("");
+
+  const handleTitleValueChange = (value) => {
+    setTitle(value);
+  };
+
+  const handleContentsValueChange = (value) => {
+    setContents(value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <TitleArea handleTitleValueChange={handleTitleValueChange} />
+      <ContentsArea handleContentsValueChange={handleContentsValueChange} />
+      <Control />
+      <pre>{title}</pre>
+      <pre>{contents}</pre>
+    </>
   );
 }
 
